91aa20ec8c865231bd3f62263cb17bb0d54c3240
Jobby tom <jobyktom@gmail.com>
2020-11-19 17:40:37 +0000

    config changes

--- packages/product-one/src/App.js
+++ packages/product-one/src/App.js
@@ -1,18 +1,23 @@
-import React from 'react';
+import React,{useState} from 'react';
 import logo from './logo.svg';
 import './App.css';
 import { Button } from 'ui-components';
 import { formatDate } from 'common-utils';
 
 function App() {
+  const [count,setCount] = useState(0)
+const handleClick = ()=>{
+    setCount(count+1)
+}
   return (
     <div className="App">
       <div>
-        Sample product
-        <Button label="A product button" />
-        <span>
+        <p>Sample product</p>
+       
+        <Button label={`A button from Ui Component ${count}`} handleClick={handleClick}/>
+        <p>
           Today is: {formatDate(new Date())}
-        </span>
+        </p>
       </div>
     </div>
   );
